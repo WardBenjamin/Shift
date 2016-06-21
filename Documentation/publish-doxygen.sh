69f32e75ee97e01f5eff0be7f1738b4ddfb585a1
Benjamin Ward <ward.programm3r@gmail.com>
2016-06-21 15:33:05 -0400

    Only publish docs if on master, publish with build number

--- Documentation/publish-doxygen.sh
+++ Documentation/publish-doxygen.sh
@@ -7,12 +7,13 @@ HTML_PATH=Documentation/html
 COMMIT_USER="Documentation Builder"
 COMMIT_EMAIL="ward.programm3r@gmail.com"
 CHANGESET=$(git rev-parse --verify HEAD)
+BUILD_NUM=$1
 
 # Create and commit the documentation repo
 cd ${HTML_PATH}
 git add .
 git config user.name "${COMMIT_USER}"
 git config user.email "${COMMIT_EMAIL}"
-git commit -m "Automated documentation build for changeset ${CHANGESET}."
+git commit -m "Automated documentation build for changeset ${CHANGESET}, Travis CI build #: ${BUILD_NUM}"
 git push origin gh-pages
 cd -
