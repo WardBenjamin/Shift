3564dd39166ce4a0d7f2e42de58bf92a8a7971eb
Benjamin Ward <ward.programm3r@gmail.com>
2016-06-21 17:03:42 -0400

    Fix missing space in documentation build script

--- Documentation/build-docs.sh
+++ Documentation/build-docs.sh
@@ -2,7 +2,7 @@
 
 if [[ $TRAVIS_BRANCH == 'master' && $TRAVIS_PULL_REQUEST == 'false' ]]
 then
-	openssl aes-256-cbc -K $encrypted_31d4dbf0621e_key -iv $encrypted_31d4dbf0621e_iv-in Documentation/travisci_rsa.enc -out Documentation/travisci_rsa -d
+	openssl aes-256-cbc -K $encrypted_31d4dbf0621e_key -iv $encrypted_31d4dbf0621e_iv -in Documentation/travisci_rsa.enc -out Documentation/travisci_rsa -d
 	chmod 0600 Documentation/travisci_rsa 
 	cp Documentation/travisci_rsa ~/.ssh/id_rsa 
 	cp Documentation/travisci_rsa.pub ~/.ssh/id_rsa.pub 
